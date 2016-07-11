918c32fc42ad91f107a993b27f1871482fdff90d
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-11 15:40:35 -0700

    Render the list of browser history urls in popup

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
+var historyData;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.active) {
-    chrome.history.search({text: '', maxResults: 10}, function(data) {
-      data.forEach(function(page) {
-        console.log(page.url);
-      });
+
+    chrome.history.search({text: '', maxResults: 5}, function(data) {
+      historyData = data;
+      // data.forEach(function(page) {
+      //   console.log(page.url);
+      // });
     });
   }  
 });
+
+
+
 
 
--- popup.js
+++ popup.js
@@ -1,8 +1,23 @@
 var renderStatus = function(statusText) {
-  // document.getElementById('status').textContent = statusText;
   $('#status').text(statusText);
 };
 
 $(function() {
-  renderStatus('jquery is working!');  
+  renderStatus('My Crumbs:');
+
+  var history = chrome.extension.getBackgroundPage().historyData;
+  var $hList = $('<ul>');
+  $.each(history, function(i) {
+    var $li = $('<li/>')
+        .appendTo($hList);
+    var $link = $('<a/>')
+        .text(history[i].title)
+        .attr('href', history[i].url)
+        .attr('target', '_blank')
+        .appendTo($li);
+  });
+  $('body').append($hList);
+
 });
+
+
