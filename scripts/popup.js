cad6f1ed1c406b2aca8ff58f26592b3fdbca35bb
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-12 15:27:45 -0700

    Add runSearch method to $scope to handle search form submission

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,43 +1,28 @@
-myApp.controller('PageController', function($scope) {
+myApp.controller('PageController', function($scope, $http) {
   $scope.message = 'Recent History:';
-  // TODO: refactor some logic out into a factory/service
+  // TODO: refactor some logic out into a factory/service if i get repetitive
   var bg = chrome.extension.getBackgroundPage();
   $scope.historyItems = bg.historyData;
+  $scope.displayedItems = $scope.historyItems;
 
   var serverUrl = bg.serverUrl;
-  // TODO: add form submitHandler method to $scope and then remove jquery
+  $scope.runSearch = function() {
+    var query = $scope.searchQuery;
+
+    $http({
+      method: 'GET',
+      url: serverUrl + '/search?q=' + query
+    })
+    .then(function (resp) {
+      $scope.displayedItems = $scope.historyItems.filter(function(historyItem) {
+        return resp.data.indexOf(historyItem.url) !== -1;
+      });
+    })
+    .catch(function(err) {
+      console.error('GET request failed', err);
+    });
+    
+
+  };
+
 });
-
-
-
-
-// $(function() {
-//   $('form').on('submit', function(e) {
-//     var query = $('#searchQuery').val();
-
-//     $.ajax({
-//       url: serverUrl + '/search?q=' + query,
-//       type: 'GET',
-//       contentType: 'application/json',
-//       success: function(data) {
-//         console.log('data from GET request: ', data);
-//         var mappedData = JSON.parse(data).map(function(link) {
-//           return 'http://' + link;
-//         });
-//         $('li').filter(function() {
-//           // return true if li's a's href is contained in mappedData (which has a leading http://)
-//           var linkHref = $(this).find('a').attr('href');
-//           return mappedData.indexOf(linkHref) === -1;
-//         }).hide('slow');
-//       },
-//       error: function(data) {
-//         console.error('GET request failed');
-//       }
-//     });
-
-//     e.preventDefault();
-//   });
-
-// });
-
-
