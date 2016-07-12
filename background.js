82a7ad550366cd74a1735ff8da894d3bff76a911
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-11 22:27:25 -0700

    Add search form that issues GET request to remote server to retrieve search results

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 var historyData;
-var postRoute = 'http://127.0.0.1:3000';
+var serverUrl = 'http://127.0.0.1:3000';
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.active) {
 
-    chrome.history.search({text: '', maxResults: 1}, function(data) {
+    chrome.history.search({text: '', maxResults: 5}, function(data) {
       historyData = data;
       data.forEach(function(page) {
         console.log(page.url);
-        // issue a POST request to 127.0.0.1:3000 with 'url=www.curltest.com'
         if (page.url.includes('stackoverflow')) {
           sendUrl(page.url);  
         }
@@ -20,7 +19,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 var sendUrl = function(url) {
   var x = new XMLHttpRequest();
-  x.open('POST', postRoute);
+  x.open('POST', serverUrl);
   var urlEncodedData = encodeURIComponent('url') + '=' + encodeURIComponent(url);
   x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   x.setRequestHeader('Content-Length', urlEncodedData.length);
--- popup.js
+++ popup.js
@@ -3,9 +3,11 @@ var renderStatus = function(statusText) {
 };
 
 $(function() {
-  renderStatus('My Crumbs:');
+  // renderStatus('My Crumbs:');
 
-  var history = chrome.extension.getBackgroundPage().historyData;
+  var bg = chrome.extension.getBackgroundPage();
+  var serverUrl = bg.serverUrl;
+  var history = bg.historyData;
   var $hList = $('<ul>');
   $.each(history, function(i) {
     var $li = $('<li/>')
@@ -17,6 +19,25 @@ $(function() {
         .appendTo($li);
   });
   $('body').append($hList);
+
+  $('form').on('submit', function(e) {
+    console.log('inside submit handler');
+    var query = $('#searchQuery').val();
+
+    $.ajax({
+      url: serverUrl + '/search?q=' + query,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('data from GET request: ', data);
+      },
+      error: function(data) {
+        console.error('GET request failed');
+      }
+    });
+
+    e.preventDefault();
+  });
 
 });
 
