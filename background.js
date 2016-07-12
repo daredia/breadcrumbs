939442e7b65a592b035c6532cb7f6d5ce10d4dc0
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-12 10:13:03 -0700

    Fetch browsing history on startup of extension, not just on load of new tab

--- background.js
+++ background.js
@@ -1,23 +1,19 @@
 var historyData;
 var serverUrl = 'http://127.0.0.1:3000';
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.active) {
-
-    chrome.history.search({text: '', maxResults: 5}, function(data) {
-      historyData = data.map(function(page) {
-        page.url = page.url.split('://')[1];
-        return page;
-      });
-      historyData.forEach(function(page) {
-        if (true || page.url.includes('stackoverflow')) { //TODO: create a blacklist of URLs
-          sendUrl(page.url);
-        }
-      });
+var getHistory = function() {
+  chrome.history.search({text: '', maxResults: 5}, function(data) {
+    historyData = data.map(function(page) {
+      page.url = page.url.split('://')[1];
+      return page;
     });
-  }  
-});
-
+    historyData.forEach(function(page) {
+      if (true || page.url.includes('stackoverflow')) { //TODO: create a blacklist of URLs
+        sendUrl(page.url);
+      }
+    });
+  });
+};
 
 var sendUrl = function(url) {
   var x = new XMLHttpRequest();
@@ -28,4 +24,10 @@ var sendUrl = function(url) {
 
 };
 
+getHistory();
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.active) {
+    getHistory();
+  }  
+});
